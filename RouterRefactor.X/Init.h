f51868b23d5a2ac452c2b913b359c28cc8c46c3c
SethCarpenter <sjc93@zips.uakron.edu>
2019-10-26 17:12:00 -0400

    Added the Code Base for the Refactor

---
+++ RouterRefactor.X/Init.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   Init.h
+ * Author: John
+ *
+ * Created on October 26, 2019, 4:10 PM
+ */
+
+#ifndef INIT_H
+#define	INIT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+    
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* INIT_H */
+
---
+++ RouterRefactor.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: John
+ *
+ * Created on October 26, 2019, 4:09 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
